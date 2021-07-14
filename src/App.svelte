bc72d0877718c6c07adb3a67ebbae80e8a92b48e
Shane Lucy <shanelucy01@protonmail.com>
2021-07-14 22:27:12 +0100

    Removed redundant code

--- src/App.svelte
+++ src/App.svelte
@@ -12,27 +12,12 @@
 		Mesh,
 		AxesHelper
 	} from 'three';
-	import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+	import { MapControls, OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 	import { onMount } from 'svelte';
-	import { setPlanets } from './planetLoader';
-	import { scaledDistances } from './scaledDistances';
+	import { calcOrbit } from './calculations';
 	import { config } from './config';
 
 	let canvas;
-	const solarSystemObjectPaths: string[] = [
-		'assets/models/sun.glb',
-		'assets/models/mercury.glb',
-		'assets/models/venus.glb',
-		'assets/models/earth.glb',
-		'assets/models/moon.glb',
-		'assets/models/mars.glb',
-		'assets/models/jupiter.glb',
-		'assets/models/saturn.glb',
-		'assets/models/uranus.glb',
-		'assets/models/neptune.glb'
-	];
-
-	const [mercuryStartX, venusStartX] = [...scaledDistances];
 
 	const scene = new Scene();
 	const camera = new PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 4500000);
@@ -47,7 +32,7 @@
 	scene.add(axes);
 
 	// scene.background = space;
-	camera.position.setZ(3000);
+	camera.position.setZ(6000);
 
 	light.position.set(100, 1000, 100);
 
@@ -59,87 +44,34 @@
 	// mesh.rotateX(20);
 	// mesh.rotateZ(0);
 	// mesh.rotateY(0);
-	const planetConfig = (planet, startX: number): any[] => {
-		const dTheta = (2 * Math.PI) / startX;
-		const theta = 0;
-		planet.scene.position.x = startX;
-		return [planet, dTheta, theta];
-	};
 	onMount(async () => {
-		// const planets = await setPlanets(solarSystemObjectPaths);
-
 		const renderer = new WebGLRenderer({
 			canvas: canvas
 		});
 		const planets = await config();
 
-		// console.log(configPlanets());
-
-		planets.forEach((planet, index) => {
-			scene.add(planets[index].data.scene);
+		planets.forEach((planet) => {
+			scene.add(planet.data.scene);
 		});
-
-		// [mercury, mercuryDTheta, mercuryTheta] = [...planetConfig(mercury, mercuryStartX)];
-		// [venus, venusDTheta, venusTheta] = [...planetConfig(venus, venusStartX)];
-		// venus.scene.position.x = venusStartX;
-		// sun.scene.scale.x = 1;
-		// sun.scene.scale.y = 1;
-		// sun.scene.scale.z = 1;
-
-		// scene.add(sun.scene);
-		// scene.add(mercury.scene);
-		// scene.add(venus.scene);
 
 		renderer.setPixelRatio(window.devicePixelRatio);
 		renderer.setSize(window.innerWidth, window.innerHeight);
-		new OrbitControls(camera, renderer.domElement);
+		const controls = new OrbitControls(camera, renderer.domElement);
+		controls.target.set(0, 5, 0);
+		controls.update();
 
 		function animate() {
 			requestAnimationFrame(animate);
-			// sun.scene.rotation.y += 0.0005;
-			// mercury.scene.rotation.y += 0.009;
+			planets.forEach((planet) => {
+				if (planet.name !== 'sun') {
+					calcOrbit(planet);
+				}
+			});
 
-			// [mercury.scene.position.x, mercury.scene.position.z, mercuryTheta] = calcOrbit(
-			// 	mercuryStartX,
-			// 	mercuryTheta,
-			// 	mercuryDTheta
-			// );
-
-			// [venus.scene.position.x, venus.scene.position.z, venusTheta] = calcOrbit(
-			// 	venusStartX,
-			// 	venusTheta,
-			// 	venusDTheta
-			// );
 			renderer.render(scene, camera);
 		}
 		animate();
 	});
-
-	const calcOrbit = (startX: number, theta: number, dTheta: number) => {
-		theta += dTheta;
-
-		const x = startX * Math.cos(theta);
-		const z = startX * Math.sin(theta);
-		return [x, z, theta];
-	};
-
-	function drawOrbitLine() {
-		let segmentCount = 32,
-			radius = 100;
-	}
-
-	/**
-	 * Planet Rotations
-	 *     Mercury – 58.6 days – 87.97 days – 10.83 km/h (6.73 mph) – 47.36 km/s (29.43 mi/s)
-    Venus – 243 days – 224.7 days – 6.52 km/h (4.5 mph) – 35.02 km/s (21.76 mi/s)
-    Earth – 1 day – 365.26 days – 1,674 km/h (1,040 mph) – 29.78 km/s (18.5 mi/s)
-    Mars – 1.03 days – 1.88 years – 866 km/h (538.1 mph) – 24 km/s (14.91 mi/s)
-    Jupiter – 0.41 days – 11.86 years – 45,583 km/h (28,324 mph) – 13.07 km/s (8.51 mi/s)
-    Saturn – 0.45 days – 29.46 years – 36,840 km/h (22,891.3 mph) – 9.68 km/s (6 mi/s)
-    Uranus – 0.72 days – 84.01 years – 14,794 km/h (9,193 mph) – 6.8 km/s (4.225 mi/s)
-    Neptune – 0.67 days – 164.79 years – 9,719 km/h (6,039 mph) – 5.43 km/s (3.374 mi/s)
-    Pluto (dwarf planet) – 6.39 days – 248.59 years – 123.21 km/h (76.56 mph) – 4.743 km/s (2.947 mi/s)
-	*/
 </script>
 
 <main>

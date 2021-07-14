43cf4126633885738f3e57df6028177f5db1de6b
Shane Lucy <shanelucy01@protonmail.com>
2021-07-14 19:22:39 +0100

    scaled distances

--- src/scaledDistances.ts
+++ src/scaledDistances.ts
@@ -1,8 +1,21 @@
-/**
- * Let's begin by testing out a 10 thousand to 1 scale
- */
-const baseDistances = [58_000_000, 108_208_000];
+import type { Distances } from './global';
 
-export const scaledDistances = baseDistances.map((distance) => {
-	return distance / 10_000;
+const baseAverageDistances: Array<Distances> = [
+	{ name: 'sun', distance: 0 },
+	{ name: 'mercury', distance: 58_000_000 },
+	{ name: 'venus', distance: 108_000_000 },
+	{ name: 'earth', distance: 146_000_000 },
+	{ name: 'moon', distance: 146_384_000 },
+	{ name: 'mars', distance: 230_000_000 },
+	{ name: 'jupiter', distance: 779_000_000 },
+	{ name: 'saturn', distance: 1_400_000_000 },
+	{ name: 'uranus', distance: 2_870_900_000 },
+	{ name: 'neptune', distance: 4_5000_000_000 }
+];
+
+export const scaledDistances: Array<Distances> = baseAverageDistances.map((value) => {
+	return {
+		name: value.name,
+		distance: value.distance / 10_000
+	};
 });

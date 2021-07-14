410bec8885b0d2a9356b06048af8dc45226511f2
Shane Lucy <shanelucy01@protonmail.com>
2021-07-14 22:21:27 +0100

    Refactored functions

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
-import { scaledDistances } from './scaledDistances';
+import { setPlanetDistances, setPlanetScales } from './scaling';
 import { setPlanets } from './planetLoader';
 import type { Planet } from './global';
 
@@ -15,22 +15,27 @@ const solarSystemObjectPaths = [
 	'assets/models/neptune.glb'
 ];
 
-export const configPlanets = async (): Promise<Array<Planet>> => {
+/**
+ * Configures planet data from gLTF files
+ */
+const configPlanets = async (): Promise<Array<Planet>> => {
 	return await setPlanets(solarSystemObjectPaths);
 };
 
-export const setPlanetDistances = async (planets: Array<Planet>): Promise<Array<Planet>> => {
-	planets.forEach((planet, index) => {
-		scaledDistances.forEach((distance) => {
-			if (planet.name === distance.name) {
-				planets[index].startX = distance.distance;
-				planets[index].data.scene.position.x = distance.distance;
-			}
-		});
+/**
+ * Sets the starting theta and dTheta values for each planet
+ */
+const setThetas = (planets: Array<Planet>): Array<Planet> => {
+	planets.forEach((planet) => {
+		planet.dTheta = (2 * Math.PI) / planet.startX;
+		planet.theta = 0;
 	});
 	return planets;
 };
 
+/**
+ * Returns the completed planet object
+ */
 export const config = async (): Promise<Array<Planet>> => {
-	return await setPlanetDistances(await configPlanets());
+	return setThetas(setPlanetScales(setPlanetDistances(await configPlanets())));
 };

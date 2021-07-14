0fc265d44c9dcdd0c7fdb59c0fc9ded27467c0a9
Shane Lucy <shanelucy01@protonmail.com>
2021-07-14 19:22:53 +0100

    Added more typing

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,6 +1,12 @@
 /// <reference types="svelte" />
 
+export interface Distances {
+	name: string;
+	distance: number;
+}
+
 export interface Planet {
 	data: GLTF;
 	name: string;
+	startX?: number;
 }

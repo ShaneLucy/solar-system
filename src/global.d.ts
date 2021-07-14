b5653381d3854117cde42153a3991d56239f0bcc
Shane Lucy <shanelucy01@protonmail.com>
2021-07-14 22:21:49 +0100

    Added more interfaces

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,12 +1,27 @@
 /// <reference types="svelte" />
+import { GLTF } from 'three/examples/jsm/loaders/GLTFLoader';
 
-export interface Distances {
+export interface BaseScales {
 	name: string;
 	distance: number;
+	radius: number;
+}
+
+export interface ScaledDistance {
+	name: string;
+	distance: number;
+}
+
+export interface ScaledDiameter {
+	name: string;
+	diameter: number;
 }
 
 export interface Planet {
 	data: GLTF;
 	name: string;
 	startX?: number;
+	theta?: number;
+	dTheta?: number;
+	scale?: number;
 }

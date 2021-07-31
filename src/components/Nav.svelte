c61eaf4a1a9d600ff1f7d7bac5830231f38b378f
Shane Lucy <shanelucy01@protonmail.com>
2021-07-31 10:08:10 +0100

    Created nav component

---
+++ src/components/Nav.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { Router, Link, Route } from 'svelte-routing';
+	import Home from '../views/SolarSystem.svelte';
+	import Sun from '../views/Sun.svelte';
+	export let url = '';
+</script>
+
+<Router {url}>
+	<nav>
+		<Link to="/">Home</Link>
+		<Link to="/sun">Sun</Link>
+	</nav>
+	<div>
+		<Route path="/sun" component={Sun} />
+		<Route path="/"><Home /></Route>
+	</div>
+</Router>
+
+<style>
+	nav {
+		font-size: 2rem;
+		display: flex;
+		flex-direction: column;
+	}
+</style>

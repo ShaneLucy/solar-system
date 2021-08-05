07a2f7181c1a1bedfbe72da345c4eeecfa814d1d
Shane Lucy <shanelucy01@protonmail.com>
2021-08-05 20:11:48 +0100

    Added loading percent & status stores

--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,6 @@
 import { writable } from 'svelte/store';
 
 export const errors = writable([]);
-
+export const loadingPercent = writable(0);
+export const loadingStatus = writable(true)
 export const loadingMessage = writable('Downloading Textures');

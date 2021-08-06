6a4e6681e68eec429e6fd5a507e540a78c7c3464
Shane Lucy <shanelucy01@protonmail.com>
2021-08-06 12:39:12 +0100

    Added routes for each planet & static head properties

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,42 @@
 <script lang="ts">
 	import { Router, Route } from 'svelte-routing';
-	import Home from './views/SolarSystem.svelte';
-	import Sun from './views/Sun.svelte';
+	import CelestialObject from './components/CelestialObject.svelte';
+	import SolarSystem from './views/SolarSystem.svelte';
 
 	export let url = '';
+	const objects = [
+		'sun',
+		'mercury',
+		'venus',
+		'earth',
+		'mars',
+		'jupiter',
+		'saturn',
+		'uranus',
+		'neptune'
+	];
 </script>
+
+<svelte:head>
+	<link rel="manifest" href={'assets/favicons/solar-system/site.webmanifest'} />
+	<link
+		rel="mask-icon"
+		href={'assets/favicons/solar-system/safari-pinned-tab.svg'}
+		color="#5bbad5"
+	/>
+	<meta name="msapplication-TileColor" content="#da532c" />
+	<meta name="theme-color" content="#ffffff" />
+</svelte:head>
 
 <Router {url}>
 	<div>
-		<Route path="/sun" component={Sun} />
-		<Route path="/"><Home /></Route>
+		<Route path="/">
+			<SolarSystem />
+		</Route>
+		{#each objects as object}
+			<Route path={`/${object}`}>
+				<CelestialObject name={object} />
+			</Route>
+		{/each}
 	</div>
 </Router>

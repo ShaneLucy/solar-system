3b4998a215876ccec4d0dd9463c915c66ceba58f
Shane Lucy <shanelucy01@protonmail.com>
2021-08-06 17:30:58 +0100

    Created component to resize canvas

---
+++ src/components/ResizeCanvas.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let camera;
+	export let renderer;
+
+	function handleResize() {
+		camera.aspect = window.innerWidth / window.innerHeight;
+		camera.updateProjectionMatrix();
+
+		renderer.setSize(window.innerWidth, window.innerHeight);
+	}
+</script>
+
+<svelte:window on:resize={() => handleResize()} />

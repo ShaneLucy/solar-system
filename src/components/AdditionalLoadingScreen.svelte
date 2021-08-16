9b4376ef00ecdcad1fc90f01b32d0866181a21f6
Shane Lucy <shanelucy01@protonmail.com>
2021-08-16 20:59:30 +0100

    Created additional object loading screen

---
+++ src/components/AdditionalLoadingScreen.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import {
+		additionalLoadingStatus,
+		loadingMessage,
+		loadingPercent,
+		loadedObjects,
+		objectsToLoad
+	} from '../store';
+</script>
+
+{#if $additionalLoadingStatus}
+	<div>
+		<ol>
+			{#each $loadedObjects as loadedObject}
+				<li>{loadedObject}</li>
+			{/each}
+		</ol>
+		<p>{$loadingMessage} {$loadingPercent}%</p>
+		<ol>
+			{#each $objectsToLoad as objectToload}
+				<li class="to-load">{objectToload}</li>
+			{/each}
+		</ol>
+	</div>
+{/if}
+
+<style>
+	div {
+		position: absolute;
+		top: 0;
+		right: 0;
+		z-index: 700;
+		display: flex;
+		flex-direction: column;
+		width: max-content;
+		height: max-content;
+		justify-content: center;
+		background-color: black;
+		color: white;
+		padding: 2rem;
+	}
+
+	.to-load {
+		color: gray;
+	}
+</style>

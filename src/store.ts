2d050af2d391edde24e64f8b79c136c085b0934f
Shane Lucy <shanelucy01@protonmail.com>
2021-08-16 21:02:04 +0100

    Created stores for additional loading screen

--- src/store.ts
+++ src/store.ts
@@ -1,6 +1,11 @@
 import { writable } from 'svelte/store';
 
 export const errors = writable([]);
+
 export const loadingPercent = writable(0);
 export const loadingStatus = writable(true);
 export const loadingMessage = writable('Downloading Textures');
+
+export const additionalLoadingStatus = writable(false);
+export const loadedObjects = writable([]);
+export const objectsToLoad = writable([]);

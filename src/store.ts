13436ef488898cd2e8045cda11f35944e3b0cc00
Shane Lucy <shanelucy01@protonmail.com>
2021-09-01 22:18:06 +0100

    Renamed store

--- src/store.ts
+++ src/store.ts
@@ -6,7 +6,7 @@ export const loadingPercent = writable(0);
 export const loadingStatus = writable(true);
 export const loadingMessage = writable('Downloading Textures');
 
-export const additionalLoadingComplete = writable(false);
+export const additionalLoadingComplete = writable(true);
 export const loadedObjects = writable([]);
 export const objectsToLoad = writable([]);
 

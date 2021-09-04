ba3a25b2109271a9fd2b042803c3711bd06c7af8
Shane Lucy <shanelucy01@protonmail.com>
2021-09-04 12:34:26 +0100

    got file to a stage where content renders, still a lot to do in here

--- src/views/SolarSystem.svelte
+++ src/views/SolarSystem.svelte
@@ -1,133 +1,181 @@
 <script lang="ts">
+	import { onMount } from 'svelte';
+	import { errors } from '../store';
+	import { loadModel } from '../loaders';
 	import {
 		Scene,
 		PerspectiveCamera,
-		TextureLoader,
 		WebGLRenderer,
 		AmbientLight,
-		// Clock,
-		RingGeometry,
-		MeshBasicMaterial,
-		GridHelper,
-		Mesh,
-		AxesHelper,
-		SphereGeometry,
-		MeshStandardMaterial,
-		MathUtils
+		sRGBEncoding,
+		Object3D,
+		Box3
 	} from 'three';
-	import { MapControls, OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-	import { onMount } from 'svelte';
+	import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+	import { loadingStatus, loadingMessage, loadingPercent } from '../store';
+	import LoadingScreen from '../components/LoadingScreen.svelte';
 	import { calcOrbit } from '../calculations';
-	import { config } from '../config';
-	import { errors, loadingMessage } from '../store';
-	import { get } from 'svelte/store';
-	import {
-		setPlanetSizeToScale,
-		setPlanetDistancesToScale,
-		setPlanetDistancesNotToScale,
-		setPlanetSizeNotToScale
-	} from '../scaling';
-
-	import HeadConfig from '../components/HeadConfig.svelte';
+	import ResizeCanvas from '../components/ResizeCanvas.svelte';
+	import type { CelestialObject, PreparedOject } from '../types/index';
+	import { completedCelestialObjects } from '../config';
 
 	let canvas;
 
 	const scene = new Scene();
-	const camera = new PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 4_500_000);
+	const camera = new PerspectiveCamera(100, window.innerWidth / window.innerHeight, 0.1, 2_000_000);
 	const light = new AmbientLight('white');
-	const grid = new GridHelper(100000, 100, 'white', 'white');
-	const axes = new AxesHelper(30000);
+	const preparedObjects: Array<PreparedOject> = [];
 
-	// const clock = new Clock(true);
-	// const space = new TextureLoader().load('assets/images/milky-way.jpg');
 	scene.add(light);
-	// scene.add(grid);
-	scene.add(axes);
+	camera.position.setZ(7_50);
+	light.position.set(100, 1_000, 100);
 
-	camera.position.setZ(4_000);
+	// let numStars = 0;
+	// while (numStars < 2_000) {
+	// 	const star = createStar(2_000_000, 400_000);
+	// 	if (star) {
+	// 		scene.add(star);
+	// 		numStars += 1;
+	// 	}
+	// }
+	let renderer;
 
-	light.position.set(100, 1000, 100);
-
-	const geometry = new RingGeometry(3000, 2000, 3000);
-	const material = new MeshBasicMaterial({ color: 0xffff00 });
-	const mesh = new Mesh(geometry, material);
-	// scene.add(mesh);
-	// mesh.position.setX(3000);
-	// mesh.rotateX(20);
-	// mesh.rotateZ(0);
-	// mesh.rotateY(0);
-	let planets;
 	onMount(async () => {
-		const renderer = new WebGLRenderer({
+		loadingStatus.set(true);
+		loadingMessage.set(`Generating Solar System`);
+		loadingPercent.set(0);
+
+		renderer = new WebGLRenderer({
 			canvas: canvas
 		});
-
-		planets = await config();
-
-		planets.forEach((planet) => {
-			try {
-				scene.add(planet.data.scene);
-			} catch (error) {
-				errors.update((val) => [...val, error]);
-			}
-		});
-
+		renderer.outputEncoding = sRGBEncoding;
 		renderer.setPixelRatio(window.devicePixelRatio);
 		renderer.setSize(window.innerWidth, window.innerHeight);
+		const sun = await loadModel(completedCelestialObjects[0].name);
+		// const planet = await loadModel(getModelFilePath(name));
+		const preparedObjects = [];
+
+		// celestialObjects.forEach(async (value) => {
+		// 	if (value.additionalObjects !== null) {
+		// 		value.additionalObjects.forEach(async (val) => {
+		// 			planet = await loadModel(getModelFilePath(val.name));
+		// 			objects.push(planet);
+		// 		});
+		// 	}
+		// 	planet = await loadModel(getModelFilePath(value.name));
+		// 	objects.push(planet);
+		// });
+
+		// loadingMessage.set('Generating Scene');
+		const object3d = new Object3D();
+		try {
+			object3d.add(sun.scene);
+			scene.add(object3d);
+		} catch (error) {
+			errors.update((val) => [...val, error]);
+		}
+
+		const boundingBox = new Box3().setFromObject(object3d);
+
+		const boundingSize = boundingBox.getSize(boundingBox.max);
+		const diameter = boundingSize.x / 2;
 		const controls = new OrbitControls(camera, renderer.domElement);
-		controls.target.set(0, 5, 0);
-		controls.update();
+
+		controls.maxDistance = 50_000;
+		controls.minDistance = 5_00;
+		controls.enableDamping = true;
+		controls.dampingFactor = 0.05;
+		controls.screenSpacePanning = false;
 
 		function animate() {
 			requestAnimationFrame(animate);
-			planets.forEach((planet) => {
-				if (planet.name !== 'sun') {
-					calcOrbit(planet);
-				}
-			});
-
 			renderer.render(scene, camera);
+			controls.update();
+			// if (preparedObjects.length > 0) {
+			// 	preparedObjects.forEach((value) => {
+			// 		calcOrbit(value);
+			// 	});
+			// }
 		}
-		animate();
-		console.log(get(errors));
-	});
 
-	let scaledSolarSystem = false;
-	async function changeScale() {
-		scaledSolarSystem = !scaledSolarSystem;
-		scaledSolarSystem
-			? setPlanetSizeToScale(setPlanetDistancesToScale(await planets))
-			: setPlanetSizeNotToScale(setPlanetDistancesNotToScale(await planets));
-	}
+		loadingStatus.set(false);
+		animate();
+
+		completedCelestialObjects.forEach(async (object, index) => {
+			if (index > 0) {
+				try {
+					const model = {
+						data: await loadModel(object.name)
+						// theta: object.theta,
+						// dTheta: object.dTheta,
+						// distanceFromPrimary: object.distanceFromPrimary
+					};
+
+					model.data.scene.scale.x = 1 / object.sizeDiffFromPrimary;
+					model.data.scene.scale.y = 1 / object.sizeDiffFromPrimary;
+					model.data.scene.scale.z = 1 / object.sizeDiffFromPrimary;
+					model.data.scene.position.x = diameter + object.distanceFromPrimary;
+
+					preparedObjects.push(model);
+
+					scene.add(model.data.scene);
+				} catch (error) {
+					errors.update((val) => [...val, error]);
+				}
+				if (object.additionalObjects !== null) {
+					object.additionalObjects.forEach(async (addObject) => {
+						try {
+							const model: PreparedOject = {
+								data: await loadModel(addObject.name),
+								theta: addObject.theta,
+								dTheta: addObject.dTheta,
+								distanceFromPrimary: addObject.distanceFromPrimary
+							};
+
+							model.data.scene.scale.x = 1 / addObject.sizeDiffFromPrimary;
+							model.data.scene.scale.y = 1 / addObject.sizeDiffFromPrimary;
+							model.data.scene.scale.z = 1 / addObject.sizeDiffFromPrimary;
+							model.data.scene.position.x = diameter + addObject.distanceFromPrimary;
+
+							preparedObjects.push(model);
+
+							scene.add(model.data.scene);
+						} catch (error) {
+							errors.update((val) => [...val, error]);
+						}
+					});
+				}
+			}
+		});
+		// if (additionalObjects !== null) {
+		// 	additionalObjects.forEach(async (object) => {
+		// try {
+		// 	const model: PreparedOject = {
+		// 		data: await loadModel(getModelFilePath(object.name)),
+		// 		theta: object.theta,
+		// 		dTheta: object.dTheta,
+		// 		distanceFromPrimary: object.distanceFromPrimary
+		// 	};
+
+		// 	model.data.scene.scale.x = 1 / object.sizeDiffFromPrimary;
+		// 	model.data.scene.scale.y = 1 / object.sizeDiffFromPrimary;
+		// 	model.data.scene.scale.z = 1 / object.sizeDiffFromPrimary;
+		// 	model.data.scene.position.x = diameter + object.distanceFromPrimary;
+
+		// 	preparedObjects.push(model);
+
+		// 	scene.add(model.data.scene);
+		// } catch (error) {
+		// 	errors.update((val) => [...val, error]);
+		// }
+		// 	});
+		// }
+	});
 </script>
 
-<HeadConfig name="solar-system" />
-<div class="loading-screen">
-	<p>{$loadingMessage}</p>
-</div>
+<ResizeCanvas {camera} {renderer} />
+<LoadingScreen />
 
 <main>
-	<button on:click={changeScale}>Change Scale</button>
 	<canvas id="background" bind:this={canvas} />
 </main>
-
-<style>
-	button {
-		position: absolute;
-		display: block;
-		z-index: 99;
-		left: 0;
-		top: 0;
-		background-color: violet;
-	}
-
-	.loading-screen {
-		display: flex;
-		width: 100%;
-		height: 100%;
-		align-items: center;
-		justify-content: center;
-		background-color: black;
-		color: white;
-	}
-</style>

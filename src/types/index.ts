dc14a49ac6229d3a2b2e6d4754d238d151293c9e
Shane Lucy <shanelucy01@protonmail.com>
2021-09-04 11:12:26 +0100

    Removed unused interfaces

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,21 +1,5 @@
 import type { GLTF } from 'three/examples/jsm/loaders/GLTFLoader';
 
-export interface BaseScales {
-	name: string;
-	distance: number;
-	radius: number;
-}
-
-export interface ScaledDistance {
-	name: string;
-	distance: number;
-}
-
-export interface ScaledDiameter {
-	name: string;
-	diameter: number;
-}
-
 export interface Planet {
 	data: GLTF;
 	name: string;

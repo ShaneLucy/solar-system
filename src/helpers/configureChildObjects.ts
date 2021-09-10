683ae29d3a37200b071190dcd05391bd1c5f4855
Shane Lucy <shanelucy01@protonmail.com>
2021-09-10 17:31:59 +0100

    Updated functions to work properly when loading solar-system

--- src/helpers/configureChildObjects.ts
+++ src/helpers/configureChildObjects.ts
@@ -9,18 +9,35 @@ import {
   showAdditionalLoader
 } from '../store';
 
-import type { AdditionalObject, PreparedOject } from '../types/index';
+import type {
+  AdditionalObject,
+  CelestialObject,
+  PreparedOject
+} from '../types/index';
 
 const showLoader = (): void => {
   additionalLoadingComplete.set(false);
   showAdditionalLoader.set(true);
 };
 
-const setObjectsToLoad = (childObjects: Array<AdditionalObject>): void => {
+const setObjectsToLoad = (
+  childObjects: Array<AdditionalObject> | Array<CelestialObject>
+): void => {
   showLoader();
-  childObjects.forEach((childObject) => {
-    objectsToLoad.update((val) => [...val, childObject.name]);
-  });
+  objectsToLoad.set(
+    childObjects.flatMap((childObject) => {
+      let completedLoad: Array<string> = [];
+
+      completedLoad = [...completedLoad, childObject.name];
+
+      if ('additionalObjects' in childObject) {
+        childObject.additionalObjects.forEach((additionalObject) => {
+          completedLoad = [...completedLoad, additionalObject.name];
+        });
+      }
+      return completedLoad;
+    })
+  );
 };
 
 const setLoadedObjects = (name: string): void => {
@@ -37,31 +54,53 @@ const hideLoader = (): void => {
   }, transitionDuation);
 };
 
+const generatePreparedObject = async (
+  object: AdditionalObject | CelestialObject
+): Promise<PreparedOject> => {
+  const model = await loadModel(object.name);
+  const object3d = new Object3D();
+
+  object3d.add(model.scene);
+  object3d.scale.set(
+    ...scalingFactor(object3d, object.radius, model.scene.scale.x)
+  );
+  object3d.position.setX(object.distanceFromPrimary);
+
+  const preparedObject: PreparedOject = {
+    data: object3d,
+    theta: object.theta,
+    dTheta: object.dTheta,
+    startX: object.distanceFromPrimary
+  };
+
+  return preparedObject;
+};
+
 export default async (
-  childObjects: Array<AdditionalObject>
+  childObjects: Array<AdditionalObject> | Array<CelestialObject>
 ): Promise<Array<PreparedOject>> => {
   setObjectsToLoad(childObjects);
 
   const preparedObjects: Array<PreparedOject> = await Promise.all(
     childObjects.map(
       async (object): Promise<PreparedOject> => {
-        const model = await loadModel(object.name);
-
-        const object3d = new Object3D();
-        object3d.add(model.scene);
-        object3d.scale.set(
-          ...scalingFactor(object3d, object.radius, model.scene.scale.x)
-        );
-        object3d.position.setX(object.distanceFromPrimary);
-
-        const preparedObject: PreparedOject = {
-          data: object3d,
-          theta: object.theta,
-          dTheta: object.dTheta,
-          startX: object.distanceFromPrimary
-        };
+        const preparedObject = generatePreparedObject(object);
 
         setLoadedObjects(object.name);
+
+        if ('additionalObjects' in object) {
+          object.additionalObjects.map(
+            async (additionalObject): Promise<PreparedOject> => {
+              const preparedChildObjects = generatePreparedObject(
+                additionalObject
+              );
+
+              setLoadedObjects(additionalObject.name);
+
+              return preparedChildObjects;
+            }
+          );
+        }
 
         return preparedObject;
       }

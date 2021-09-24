a260864883b16c4f86e092202c84d18252e55c25
Shane Lucy <shanelucy01@protonmail.com>
2021-09-24 20:58:00 +0100

    Integrated my eslint plugins

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,5 @@
 module.exports = {
-  root: true,
-  parser: '@typescript-eslint/parser',
-  extends: [
-    'airbnb-base',
-    'airbnb-typescript/base',
-    'prettier',
-    'plugin:@ota-meshi/svelte/recommended'
-  ],
-  plugins: ['svelte3', '@typescript-eslint'],
-  ignorePatterns: ['*.cjs, **/*.js'],
-  overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-  settings: {
-    'svelte3/typescript': () => require('typescript')
-  },
-  parserOptions: {
-    sourceType: 'module',
-    ecmaVersion: 2019,
-    project: './tsconfig.json'
-  },
-  env: {
-    browser: true,
-    es2017: true,
-    node: true
-  },
+  extends: 'typescript-airbnb-prettier-svelte',
   rules: {
     'import/no-extraneous-dependencies': 0
   }
